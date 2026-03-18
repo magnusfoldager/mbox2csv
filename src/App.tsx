ef706848e6eadf6b8a1b8dec7f65f54b4dc8697b
Magnus Foldager <4417002+magnusfoldager@users.noreply.github.com>
2026-03-18 08:49:30 +0100

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className='bg-zinc-200 dark:bg-zinc-950 min-h-screen flex items-center justify-center'>
+      <div className="max-w-sm bg-zinc-50 dark:bg-zinc-900 p-6 rounded-lg shadow-md border border-zinc-200 dark:border-zinc-700">
+        <h1 className="text-2xl font-bold text-zinc-900 dark:text-zinc-100">mbox2csv</h1>
+      </div>
+
+    </main>
+  )
+}
+
+export default App
